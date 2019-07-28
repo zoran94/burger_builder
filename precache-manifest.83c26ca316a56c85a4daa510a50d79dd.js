2a3d328c96e4af49aacb154a76d7bede148571a6
zoran94 <zorans94@hotmail.com>
2019-07-28 12:19:38 +0200

    Updates

---
+++ precache-manifest.83c26ca316a56c85a4daa510a50d79dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bfc4e110b5a780407494b7bfc3e8a5",
+    "url": "/burger_builder_app/index.html"
+  },
+  {
+    "revision": "ad8b0e832df7d1dbc73e",
+    "url": "/burger_builder_app/static/css/main.3f849aaf.chunk.css"
+  },
+  {
+    "revision": "670d7e4cf8e82ae9a54a",
+    "url": "/burger_builder_app/static/js/2.501cf1ec.chunk.js"
+  },
+  {
+    "revision": "ad8b0e832df7d1dbc73e",
+    "url": "/burger_builder_app/static/js/main.4d6eea2d.chunk.js"
+  },
+  {
+    "revision": "a746f42e1a9235357356",
+    "url": "/burger_builder_app/static/js/runtime~main.c30eb395.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger_builder_app/static/media/27.1 burger-logo.png.b8503d26.png"
+  }
+]);
